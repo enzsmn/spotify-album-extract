0d0cb4f12fb643f873925914ba8227119e6aea1f
Enzo <enzosimoen@gmail.com>
2020-02-15 14:03:57 +0100

    Copyright in footer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
     <div id="app" class="container">
         <router-view></router-view>
+        <a href="https://nozem.space" target="_blank" class="copyright muted small">&copy; {{ year }} nozem</a>
     </div>
 </template>
 
 <script>
+    import moment from 'moment';
+
     export default {
         name: 'app',
+        computed: {
+            year() {
+                return moment().format('Y');
+            },
+        }
     }
 </script>
 
