c482a9d3ddbd238b132571b4c28fb5e1038b2162
Enzo Simoen <enzosimoen@gmail.com>
2021-05-19 18:46:36 +0200

    Typo

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,7 @@
 
         <h1>Spotify <span>album extract</span></h1>
 
-        <p>This tool allows you to put the full abums from a selected playlist's tracks into a new playlist.</p>
+        <p>This tool allows you to put the full albums from a selected playlist's tracks into a new playlist.</p>
 
         <b-button type="is-primary is-rounded" @click="auth">Authorize and load playlists</b-button>
 
