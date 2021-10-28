3afc36096efb30cfd92e6631decc0c17d7676c5a
Enzo Simoen <enzosimoen@gmail.com>
2021-10-28 11:47:28 +0200

    Fix cached token validity

--- src/components/Callback.vue
+++ src/components/Callback.vue
@@ -15,8 +15,7 @@
 
             console.log('Callback', this.$route.query);
 
-            const expiration = this.$route.query.code / 60;
-            lscache.set('spotify_auth_code', this.$route.query.code, expiration);
+            lscache.set('spotify_auth_code', this.$route.query.code, 60);
 
             this.getToken(this.$route.query.code);
         },
