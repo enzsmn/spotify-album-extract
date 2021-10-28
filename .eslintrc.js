1d4374a092ad69114a6c7cdc4f8d22bb94724efe
Enzo Simoen <enzosimoen@gmail.com>
2021-10-28 14:40:47 +0200

    Add linting

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ["plugin:vue/recommended", "eslint:recommended", "@vue/prettier"],
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+  plugins: ["simple-import-sort"],
+  rules: {
+    curly: 1,
+    eqeqeq: "warn",
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "simple-import-sort/imports": [
+      "warn",
+      {
+        groups: [["^\\u0000", "^@?\\w", "^[^.]", "^\\."]]
+      }
+    ]
+  }
+};
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ],
-  plugins: process.env.VUE_APP_ENV === 'production' ? ['transform-remove-console'] : []
-}
+  presets: ["@vue/cli-plugin-babel/preset"],
+  plugins:
+    process.env.VUE_APP_ENV === "production" ? ["transform-remove-console"] : []
+};
