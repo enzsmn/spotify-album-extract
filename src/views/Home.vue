07d79ab901a9788bae4773c15e27a8e42fa6b629
Enzo Simoen <10513973+enzsmn@users.noreply.github.com>
2023-12-29 08:07:03 +0100

    Change copy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
     <p>Select a playlist and get the full album from each track.</p>
 
     <b-button type="is-primary is-rounded" @click="auth">
-      Authorize and load playlists
+      Load your playlists
     </b-button>
   </main>
 </template>
